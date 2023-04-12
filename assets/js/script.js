85de9c3a8eeecbb7a128198eba9efdcd891ba5cb
AntimatterNova <dmvsecondary@gmail.com>
2023-04-12 02:41:05 -0500

    added question/answer library to the JS, added css styling rules for .timer .question and .answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,68 @@
-var timeEl = document.querySelector(".timer");
-var gameField = document.querySelector(".play-field");
-var startBtn = document.querySelector(".startBtn");
-var question = document.querySelector(".question");
+var questionList = [{
+    questionText: 'What does "var" do in a Javascript file?',
+    choices: ['It assigns a value to a name', 'It changes the text in a selected HTMl element', 'It logs a selected value in the browser console',],
+    trueAns: 'It assigns a value to a name',
+  },
+  {
+    questionText: '',
+    choices: [],
+    trueAns: '',
+  },
+  {
+    questionText: '',
+    choices: [],
+    trueAns: '',
+  },
+  {
+    questionText: '',
+    choices: [],
+    trueAns: '',
+  },
+];
 
-var secondsLeft = 120;
+var timeEl = document.querySelector('.timer');
+var gameField = document.querySelector('.play-field');
+var startBtn = document.querySelector('.startBtn');
+var questionBox = document.querySelector('.question');
+var answerBox = document.querySelector('.answers');
+var curQuestion = 0;
+var secLeft = 60;
+var score = 0;
 
-// startBtn.addEventListener('click', function setTime() {
-//     console.log('button was pressed');
+function main() {
+  questionBox.textContent = questionList[0].questionText;
 
-//     var timerInterval = setInterval(function() {
+  for (var i = 0; i < questionList[0].choices.length; i++) {
+    var genButton = document.createElement('button');
+    genButton.textContent = questionList[0].choices[i];
+    answerBox.append(genButton);
+  };
 
-//       secondsLeft--;
-//       timeEl.textContent = 'Time left: ' + secondsLeft;
-  
-//       if(secondsLeft === 0) {
-//         // Stops execution of action at set interval
-//         clearInterval(timerInterval);
-//         // Calls function to create and append image
-//         };
-//     }, 1000);
-// });
+  if (genButton.textContent = questionList[].trueAns) {
+    score + 10;
+  } else {
+    secLeft - 5;
+  };
+  console.log(secLeft);
+};
+
+main();
+console.log('test');
+
 
 startBtn.addEventListener('click', function(event) {
-    console.log('Game started');
-    event.stopPropagation();
-
-    startBtn.remove();
-    
-
-    var timerInterval = setInterval(function() {
-
-      secondsLeft--;
-      timeEl.textContent = 'Time left: ' + secondsLeft;
+  console.log('Game started');
+  startBtn.remove();
   
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        };
-    }, 1000);
+  var timerInterval = setInterval(function() {
+
+    secLeft--;
+    timeEl.textContent = 'Time left: ' + secLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      };
+  }, 1000);
 });
